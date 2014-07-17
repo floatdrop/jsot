f6ae1764e1dcb1e84798b136a51e851bb52762a1
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-07-18 00:19:49 +0600

    Added benchmark for isMatching function
    It could be faster, I think

---
+++ benchmark/is_matching.js
@@ -0,0 +1,27 @@
+/* global suite, bench, set */
+
+var isMatching = require('..').prototype.isMatching;
+
+suite('isMatching', function () {
+    bench('simple values', function () {
+        return isMatching('a', 'a') === true;
+    });
+
+    bench('simple objects', function () {
+        return isMatching({a: 1}, {a: 1, b: 1}) === true;
+    });
+
+    bench('bh object ', function () {
+        return isMatching(
+            { block: 'p', blockMods: { bold: 'yes' } },
+            { block: 'p', blockMods: { bold: 'yes' }, content: 'some' }
+        ) === true;
+    });
+
+    bench('bh complex object ', function () {
+        return isMatching(
+            { block: 'p', blockMods: { bold: 'yes' }, elem: 'a', mods: { italic: true } },
+            { block: 'p', blockMods: { bold: 'yes' }, elem: 'a', mods: { italic: true }, content: 'some' }
+        ) === true;
+    });
+});
