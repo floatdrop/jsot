22edacba334758e0dd1e33f039b858320143bec4
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-07-19 00:07:36 +0600

    staticMatching benchmark

---
+++ benchmark/staticMatching.js
@@ -0,0 +1,54 @@
+/* global suite, bench */
+
+function isMatching (a, b) {
+    if (a.block !== b.block) { return false; }
+
+    if (a.blockMods) {
+        if (!b.blockMods) { return false; }
+
+        for (var mod in a.blockMods) {
+            if (a.blockMods[mod] !== b.blockMods[mod]) { return false; }
+        }
+    }
+
+    if (a.elem) {
+        if (a.elem !== b.elem) { return false; }
+
+        if (a.elemMods) {
+            if (!b.elemMods) { return false; }
+
+            for (var emod in a.elemMods) {
+                if (a.elemMods[emod] !== b.elemMods[emod]) { return false; }
+            }
+        }
+    }
+
+    return true;
+}
+
+suite('staticFunction', function () {
+    bench('block', function () {
+        return isMatching({ block: 'a' }, { block: 'a' }) === true;
+    });
+
+    bench('block_mod', function () {
+        return isMatching(
+            { block: 'a', blockMods: { one: 'yes' } },
+            { block: 'a', blockMods: { one: 'yes', two: 'yiss' } }
+        ) === true;
+    });
+
+    bench('block__elem', function () {
+        return isMatching(
+            { block: 'a', elem: 'e' },
+            { block: 'a', elem: 'e' }
+        ) === true;
+    });
+
+    bench('block_mod__elem_mod', function () {
+        return isMatching(
+            { block: 'p', blockMods: { bold: 'yes' }, elem: 'a', elemMods: { italic: true } },
+            { block: 'p', blockMods: { bold: 'yes' }, elem: 'a', elemMods: { italic: true } }
+        ) === true;
+    });
+});
